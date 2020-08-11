23b51b0db22fb83738f8895055c5335b119c6b2c
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-12 00:01:23 +0800

    Update index.php
    Change paragraph from "d4dendel" to "hapinoworries"

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
 </head>
 <body>
-	<p><b>D</b><b style="color: #267151;">4</b><b>D</b>endel</p>
+	<p><b>HA</b><b style="color: #267151;">PINO</b>WORRIES</p>
 	<img src="d4dendel (1).jpg">
 	<img src="d4dendel (2).jpg">
 	<img src="d4dendel (3).jpg">
