755d0512dd670758603fbb182134f44fc89a3cbd
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-11 23:55:32 +0800

    Added files via upload
    My art gallery

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dendel Pino</title>
+	<link rel="stylesheet" type="text/css" href="photo.css">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
+</head>
+<body>
+	<p><b>D</b><b style="color: #267151;">4</b><b>D</b>endel</p>
+	<img src="d4dendel (1).jpg">
+	<img src="d4dendel (2).jpg">
+	<img src="d4dendel (3).jpg">
+
+	<img src="d4dendel (4).jpg">
+	<img src="d4dendel (5).jpg">
+	<img src="d4dendel (6).jpg">
+
+	<img src="d4dendel (7).jpg">
+	<img src="d4dendel (8).jpg">
+	<img src="d4dendel (9).jpg">
+
+	<img src="d4dendel (10).jpg">
+	<img src="d4dendel (11).jpg">
+</body>
+</html>
