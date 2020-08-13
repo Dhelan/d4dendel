35829f318801e03d7c022287e6adab4abdcd178e
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 13:40:12 +0800

    Update blog.php

--- blog.php
+++ blog.php
@@ -14,6 +14,19 @@
 	<link rel="stylesheet" type="text/css" href="">
 </head>
 <body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">HAPINOWORRIES</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li><a href="#">HOME</a></li>
+	      <li class="active"><a href="#">BLOG</a></li>
+	      <li><a href="#">GALLERY</a></li>
+	      <li><a href="#">ABOUT</a></li>
+	    </ul>
+	  </div>
+	</nav>
 	<div>
 		<h2>The HTML code that I used in this blog page</h2>
 		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing. <code></code></p>
