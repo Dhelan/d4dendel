19ebc9b054ec00329282d19fe1a09202b84eb221
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-14 01:32:03 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
-<?php include 'vars.php'; ?>
+<div class="topnav" id="myTopnav">
+	  <a <?php if($_SERVER['SCRIPT_NAME']=="/index.php") { ?>  class="active"   <?php   }  ?> href="index.php">HA<b>PINO</b>WORRIES</a>
+	  <a <?php if($_SERVER['SCRIPT_NAME']=="/blog.php") { ?>  class="active"   <?php   }  ?> href="blog.php">BLOG</a>
+	  <a <?php if($_SERVER['SCRIPT_NAME']=="/gallery.php") { ?>  class="active"   <?php   }  ?> href="gallery.php">GALLERY</a>
+	  <a <?php if($_SERVER['SCRIPT_NAME']=="/about.php") { ?>  class="active"   <?php   }  ?> href="#about">ABOUT</a>
+	  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+	    <i class="fa fa-bars"></i>
+	  </a>
+</div>
