5d4bbe79f326b2b3007216b0c1445dc1ca90f2ec
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 15:33:52 +0800

    Update blog.php

--- blog.php
+++ blog.php
@@ -36,34 +36,36 @@
 		<div class="post span-8">
 			<h2 class="date">LESSON 7 | August 8-14</h2>
 			<h1>Sharing the Word</h1>
-			<img src="gless07.jpg" alt="Girl in a jacket" width="500">
+			<center>
+				<img src="gless07.jpg" alt="Girl in a jacket" width="500">
+			</center>
 			<p><b>Memory Text: </b>“So shall My word be that goes forth from My mouth; it shall not return to Me void, but it shall accomplish what I please, and it shall prosper in the thing for which I sent it” <b>(Isaiah 55:11, NKJV).</b></q></p?
 		</div>
 		<div class="post">
 			<div class="date">Sunday | August 9</div>
 			<h2>Symbols of God’s Word</h2>
-			<p>The varied symbols used in these passages describe some of the primary functions of the Word of God. When we share the Scriptures with others, it is like a light that illuminates life. Jesus, “the light of the world”, breaks through the darkness of their misunderstanding about who God is and the nature of His character. Minds darkened with a misunderstanding of God are illuminated by the Holy Spirit through the Word of God.
+			<p>The varied symbols used in these passages describe some of the primary functions of the Word of God. When we share the Scriptures with others, it is like a light that illuminates life. Jesus, “the light of the world”, breaks through the darkness of their misunderstanding about who God is and the nature of His character. Minds darkened with a misunderstanding of God are illuminated by the Holy Spirit through the Word of God.</p>
 
-			According to Jeremiah, the Word of God is like a fire and a hammer. It consumes the dross of sin in our lives and breaks our hard hearts. When we help people see in Scripture the glory of Jesus, their hard hearts are broken, and the fire of His love consumes the dross of selfishness, greed, lust, and self-centeredness.
+			<p>According to Jeremiah, the Word of God is like a fire and a hammer. It consumes the dross of sin in our lives and breaks our hard hearts. When we help people see in Scripture the glory of Jesus, their hard hearts are broken, and the fire of His love consumes the dross of selfishness, greed, lust, and self-centeredness.</p>
+			</p>
+			<p>The Word of God is also likened to seed. The key characteristic of seed is that it is life-giving. Seed takes time to grow. Not all seeds germinate at the same time. Not all plants grow at the same rate. But under the right conditions, the life in the seed bursts forth through the soil into new life. When we plant the seed of the Word of God in the hearts and minds of others, we will not always see immediate results, but silently the seed is growing, and in God’s own time, if they respond to the Holy Spirit’s promptings, it will produce a harvest for God’s kingdom.</p>
 
-			The Word of God is also likened to seed. The key characteristic of seed is that it is life-giving. Seed takes time to grow. Not all seeds germinate at the same time. Not all plants grow at the same rate. But under the right conditions, the life in the seed bursts forth through the soil into new life. When we plant the seed of the Word of God in the hearts and minds of others, we will not always see immediate results, but silently the seed is growing, and in God’s own time, if they respond to the Holy Spirit’s promptings, it will produce a harvest for God’s kingdom.
+			<p>Jesus likens His word to nourishing bread. As many of us know, there are few things as satisfying as a good loaf of bread. The Word of God satisfies the hunger of the soul and nourishes our inner spiritual longings. As you share the promises of the Word with others and help them discover that Jesus is the Word, their lives will be transformed by His goodness, charmed by His love, amazed at His grace, and satisfied in His presence.</p>
 
-			Jesus likens His word to nourishing bread. As many of us know, there are few things as satisfying as a good loaf of bread. The Word of God satisfies the hunger of the soul and nourishes our inner spiritual longings. As you share the promises of the Word with others and help them discover that Jesus is the Word, their lives will be transformed by His goodness, charmed by His love, amazed at His grace, and satisfied in His presence.
-
-			<b>Again, think about the truths that we know only from the Bible. What should this tell us about how much we should treasure what it teaches us?</b></p>
+			<p><b>Again, think about the truths that we know only from the Bible. What should this tell us about how much we should treasure what it teaches us?</b></p>
 		</div>
 		<div class="post">
 			<div class="date">Monday | August 10</div>
 			<h2>Symbols of God’s Word</h2>
-			<p>The Word of God is the Living Word. It carries with it the power to accomplish the things that it declares. Human words can speak of what is, but God speaks of things that are not yet done and then creates them by the power of His word. The Word of God is a creative Word. The audible Word that proceeds from His mouth has the power to create everything that it proclaims.
+			<p>The Word of God is the Living Word. It carries with it the power to accomplish the things that it declares. Human words can speak of what is, but God speaks of things that are not yet done and then creates them by the power of His word. The Word of God is a creative Word. The audible Word that proceeds from His mouth has the power to create everything that it proclaims.</p>
 
-			In the Creation story of Genesis 1, the expression “God said” is used repeatedly (Gen. 1:3, 6, 11, 14, 20, 24, 26, 29). God’s declarative words had such power that when He spoke, dry land appeared, plants sprouted, flowers blossomed, fruit trees flourished, and animals sprang forth.
+			<p>In the Creation story of Genesis 1, the expression “God said” is used repeatedly (Gen. 1:3, 6, 11, 14, 20, 24, 26, 29). God’s declarative words had such power that when He spoke, dry land appeared, plants sprouted, flowers blossomed, fruit trees flourished, and animals sprang forth.</p>
 
-			There is a fascinating Hebrew word used in Genesis 1 for the creative activity of God. It is the word bara. In this particular form, it is used of God’s activity to create something from nothing. The verb is used only when God is the subject. That is, only God alone can bara, and He does so through the power of His spoken word.
+			<p>There is a fascinating Hebrew word used in Genesis 1 for the creative activity of God. It is the word bara. In this particular form, it is used of God’s activity to create something from nothing. The verb is used only when God is the subject. That is, only God alone can bara, and He does so through the power of His spoken word.</p>
 
-			God not only created this world through the power of His word, but He sustains and upholds it through His word, as well. The same power that is in the spoken Word of God is in His written word. The same Holy Spirit that was active in Creation was active in inspiring Scripture. He is present when we read the Bible or share it with others. There is life-giving, life-changing, creative power in the Word of God. “The creative energy that called the worlds into existence is in the word of God. This word imparts power; it begets life. Every command is a promise; accepted by the will, received into the soul, it brings with it the life of the Infinite One. It transforms the nature and re-creates the soul in the image of God”. — Ellen G. White, Education, p. 126.
+			<p>God not only created this world through the power of His word, but He sustains and upholds it through His word, as well. The same power that is in the spoken Word of God is in His written word. The same Holy Spirit that was active in Creation was active in inspiring Scripture. He is present when we read the Bible or share it with others. There is life-giving, life-changing, creative power in the Word of God. “The creative energy that called the worlds into existence is in the word of God. This word imparts power; it begets life. Every command is a promise; accepted by the will, received into the soul, it brings with it the life of the Infinite One. It transforms the nature and re-creates the soul in the image of God”. — Ellen G. White, Education, p. 126.</p>
 
-			As we personally grasp the promises found in the Word of God, our lives are changed, and as we help others to grasp these amazing promises, the Holy Spirit will change their lives, too.
+			<p>As we personally grasp the promises found in the Word of God, our lives are changed, and as we help others to grasp these amazing promises, the Holy Spirit will change their lives, too.
 
 			<b>Imagine: God spoke and it was. How can we grasp what this means? What does this amazing reality tell us about His power? Why should this truth about God’s creative power be comforting to us?</b></p>
 		</div>
