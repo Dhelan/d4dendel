112f52d00d045acd407dcef4c58e62aaff00ab85
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-14 01:35:53 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Dendel Pino</title>
 	<link rel="stylesheet" type="text/css" href="photo.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -12,19 +13,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <a class="navbar-brand" href="index.php"><p>HA<b style="color: #267151;">PINO</b>WORRIES</p></a>
-	    </div>
-	    <ul class="nav navbar-nav">
-	      <li><a href="index.php">HOME</a></li>
-	      <li><a href="blog.php">BLOG</a></li>
-	      <li class="active"><a href="gallery.php">GALLERY</a></li>
-	      <li><a href="about.php">ABOUT</a></li>
-	    </ul>
-	  </div>
-	</nav>
+	<?php include 'header.php'; ?>
 	<div class="container" style="padding-top: 70px;">
 	   <img src="d4dendel (1).jpg">
 	   <img src="d4dendel (2).jpg">
