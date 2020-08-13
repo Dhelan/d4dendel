91dde8059107c9e463e482c0e9b0188aadcbc395
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 21:20:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,31 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<style>
-	  .container-fluid-1 {
+	<style>  
+	  body {
+	    font: 20px Montserrat, sans-serif;
+	    line-height: 1.8;
+	    color: #f5f6f7;
+	  }
+	  p {font-size: 16px;}
+	  .margin {margin-bottom: 45px;}
+	  .bg-1 { 
+	    background-color: #1abc9c; /* Green */
+	    color: #ffffff;
+	  }
+	  .bg-2 { 
+	    background-color: #474e5d; /* Dark Blue */
+	    color: #ffffff;
+	  }
+	  .bg-3 { 
+	    background-color: #ffffff; /* White */
+	    color: #555555;
+	  }
+	  .bg-4 { 
+	    background-color: #2f2f2f; /* Black Gray */
+	    color: #fff;
+	  }
+	  .container-fluid {
 	    padding-top: 70px;
 	    padding-bottom: 70px;
 	  }
