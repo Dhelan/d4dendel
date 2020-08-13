83c1196d5e9c21aec0492f2a5b210adb7eff7a21
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 23:48:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -103,6 +103,10 @@
 	  </a>
 	</div>
 	<!-- First Container -->
+	<!-- The video -->
+	<video autoplay muted loop id="myVideo">
+	  <source src="https://www.youtube.com/watch?v=MbxABgxA1z0" type="video/mp4">
+	</video>
 	<div class="container-fluid container-fluid-1 bg-1 text-center" >
 		<div class="row">
 			<div class="col-lg-12">
