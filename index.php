031dbc5fdd62eaabc9cbca10ffdc543018f24b99
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 14:07:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,20 +27,21 @@
 	    </ul>
 	  </div>
 	</nav>
-	
-	<img src="d4dendel (1).jpg">
-	<img src="d4dendel (2).jpg">
-	<img src="d4dendel (3).jpg">
+	<div class="container">
+	   <img src="d4dendel (1).jpg">
+	   <img src="d4dendel (2).jpg">
+	   <img src="d4dendel (3).jpg">
 
-	<img src="d4dendel (4).jpg">
-	<img src="d4dendel (5).jpg">
-	<img src="d4dendel (6).jpg">
+	   <img src="d4dendel (4).jpg">
+	   <img src="d4dendel (5).jpg">
+	   <img src="d4dendel (6).jpg">
 
-	<img src="d4dendel (7).jpg">
-	<img src="d4dendel (8).jpg">
-	<img src="d4dendel (9).jpg">
+	   <img src="d4dendel (7).jpg">
+	   <img src="d4dendel (8).jpg">
+	   <img src="d4dendel (9).jpg">
 
-	<img src="d4dendel (10).jpg">
-	<img src="d4dendel (11).jpg">
+	   <img src="d4dendel (10).jpg">
+	   <img src="d4dendel (11).jpg">
+	</div>
 </body>
 </html>
