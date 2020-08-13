f7effcfb2927302978966c7487e649647bcfa8aa
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 15:40:44 +0800

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dendel Pino</title>
+	<link rel="stylesheet" type="text/css" href="photo.css">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="index.php"><p>HA<b style="color: #267151;">PINO</b>WORRIES</p></a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li><a href="#">HOME</a></li>
+	      <li><a href="blog.php">BLOG</a></li>
+	      <li class="active"><a href="gallery.php">GALLERY</a></li>
+	      <li><a href="about.php">ABOUT</a></li>
+	    </ul>
+	  </div>
+	</nav>
+	<div class="container" style="padding-top: 70px;">
+	   <img src="d4dendel (1).jpg">
+	   <img src="d4dendel (2).jpg">
+	   <img src="d4dendel (3).jpg">
+	   <img src="d4dendel (4).jpg">
+	   <img src="d4dendel (5).jpg">
+	   <img src="d4dendel (6).jpg">
+	   <img src="d4dendel (7).jpg">
+	   <img src="d4dendel (8).jpg">
+	   <img src="d4dendel (9).jpg">
+	   <img src="d4dendel (10).jpg">
+	   <img src="d4dendel (11).jpg">
+	</div>
+</body>
+</html>
