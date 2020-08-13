b8c5e63ae9c6b60c820ef80f85ffb34926f11e65
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 13:42:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
 	<title>Dendel Pino</title>
 	<link rel="stylesheet" type="text/css" href="photo.css">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<p>HA<b style="color: #267151;">PINO</b>WORRIES</p>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#"><p>HA<b style="color: #267151;">PINO</b>WORRIES</p></a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li><a href="#">HOME</a></li>
+	      <li class="active"><a href="#">BLOG</a></li>
+	      <li><a href="#">GALLERY</a></li>
+	      <li><a href="#">ABOUT</a></li>
+	    </ul>
+	  </div>
+	</nav>
+	
 	<img src="d4dendel (1).jpg">
 	<img src="d4dendel (2).jpg">
 	<img src="d4dendel (3).jpg">
