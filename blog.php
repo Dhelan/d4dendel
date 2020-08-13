f59d504d6eac49d7ceb6cb8bbb9bae01bac524ea
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 13:30:46 +0800

    Added blog.php and blog.css
    Added blog.php and blog.css

---
+++ blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hapinoworries</title>
+	<link rel="stylesheet" type="text/css" href="blog.css">
+</head>
+<body>
+	<div>
+		<h2>The HTML code that I used in this blog page</h2>
+		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing. <code><!DOCTYPE html>
+<html>
+<head>
+	<title>Hapinoworries</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+	<div>
+		<h2>The HTML code that I used in this blog page</h2>
+		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing. <code></code></p>
+		<h2>The HTML code that I used in this blog page</h2>
+		<p>Bellow is the css code that I used in making this blog article.</p>
+	</div>
+</body>
+</html></code></p>
+		<h2>The HTML code that I used in this blog page</h2>
+		<p>Bellow is the css code that I used in making this blog article.</p>
+	</div>
+</body>
+</html>
