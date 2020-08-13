55cdc14ac11d017d0688e2fe6f939cee927e391f
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 14:25:15 +0800

    Update blog.php

--- blog.php
+++ blog.php
@@ -14,19 +14,10 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div>
-		<h2>The HTML code that I used in this blog page</h2>
-		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing. <code><!DOCTYPE html>
-<html>
-<head>
-	<title>Hapinoworries</title>
-	<link rel="stylesheet" type="text/css" href="">
-</head>
-<body>
 	<nav class="navbar navbar-default navbar-fixed-top">
 	  <div class="container-fluid">
 	    <div class="navbar-header">
-	      <a class="navbar-brand" href="#">HAPINOWORRIES</a>
+	      <a class="navbar-brand" href="index.php"><p>HA<b style="color: #267151;">PINO</b>WORRIES</p></a>
 	    </div>
 	    <ul class="nav navbar-nav">
 	      <li><a href="index.php">HOME</a></li>
@@ -36,14 +27,9 @@
 	    </ul>
 	  </div>
 	</nav>
-	<div>
+	<div class = "container">
 		<h2>The HTML code that I used in this blog page</h2>
-		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing. <code></code></p>
-		<h2>The HTML code that I used in this blog page</h2>
-		<p>Bellow is the css code that I used in making this blog article.</p>
-	</div>
-</body>
-</html></code></p>
+		<p>This is the article paragraph that I use to make this sample blog article, and bellow this paragraph is where you can see my html code for this blog that you are seeing.</p>
 		<h2>The HTML code that I used in this blog page</h2>
 		<p>Bellow is the css code that I used in making this blog article.</p>
 	</div>
