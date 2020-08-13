606d957babdcef4f7e1fae5b7a9d45df51131c2c
Dhelan <35637363+Dhelan@users.noreply.github.com>
2020-08-13 15:50:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>Dendel Pino</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="photo.css">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;800&display=swap" rel="stylesheet">
 	<!-- Latest compiled and minified CSS -->
@@ -12,6 +14,35 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<style>
+	  body {
+	    font: 20px Montserrat, sans-serif;
+	    line-height: 1.8;
+	    color: #f5f6f7;
+	  }
+	  p {font-size: 16px;}
+	  .margin {margin-bottom: 45px;}
+	  .bg-1 { 
+	    background-color: #1abc9c; /* Green */
+	    color: #ffffff;
+	  }
+	  .bg-2 { 
+	    background-color: #474e5d; /* Dark Blue */
+	    color: #ffffff;
+	  }
+	  .bg-3 { 
+	    background-color: #ffffff; /* White */
+	    color: #555555;
+	  }
+	  .bg-4 { 
+	    background-color: #2f2f2f; /* Black Gray */
+	    color: #fff;
+	  }
+	  .container-fluid {
+	    padding-top: 70px;
+	    padding-bottom: 70px;
+	  }
+	 </style>
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top">
@@ -27,21 +58,44 @@
 	    </ul>
 	  </div>
 	</nav>
-	<div class="container" style="padding-top: 70px;">
-	   <img src="d4dendel (1).jpg">
-	   <img src="d4dendel (2).jpg">
-	   <img src="d4dendel (3).jpg">
-
-	   <img src="d4dendel (4).jpg">
-	   <img src="d4dendel (5).jpg">
-	   <img src="d4dendel (6).jpg">
-
-	   <img src="d4dendel (7).jpg">
-	   <img src="d4dendel (8).jpg">
-	   <img src="d4dendel (9).jpg">
-
-	   <img src="d4dendel (10).jpg">
-	   <img src="d4dendel (11).jpg">
+	<!-- First Container -->
+	<div class="container-fluid bg-1 text-center">
+	  <h3 class="margin">Who Am I?</h3>
+	  <img src="d4dendel (5).jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
+	  <h3>I'm an adventurer</h3>
 	</div>
+
+	<!-- Second Container -->
+	<div class="container-fluid bg-2 text-center">
+	  <h3 class="margin">What Am I?</h3>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+	  <a href="#" class="btn btn-default btn-lg">
+	    <span class="glyphicon glyphicon-search"></span> Search
+	  </a>
+	</div>
+
+	<!-- Third Container (Grid) -->
+	<div class="container-fluid bg-3 text-center">    
+	  <h3 class="margin">Where To Find Me?</h3><br>
+	  <div class="row">
+	    <div class="col-sm-4">
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	      <img src="d4dendel (6).jpg" class="img-responsive margin" style="width:100%" alt="Image">
+	    </div>
+	    <div class="col-sm-4"> 
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	      <img src="d4dendel (9).jpg" class="img-responsive margin" style="width:100%" alt="Image">
+	    </div>
+	    <div class="col-sm-4"> 
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	      <img src="d4dendel (8).jpg" class="img-responsive margin" style="width:100%" alt="Image">
+	    </div>
+	  </div>
+	</div>
+
+	<!-- Footer -->
+	<footer class="container-fluid bg-4 text-center">
+	  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
+	</footer>
 </body>
 </html>
